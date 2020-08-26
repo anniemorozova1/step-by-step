9baecbcdb7d3f23970c4490deaf06eae6109e7f9
user <user@MacBook-Air-user.local>
2020-08-26 21:40:50 +0300

    Thunder

--- rn-step-by-step/App.js
+++ rn-step-by-step/App.js
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Image, TouchableHighlight } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <View style={styles.navbar}></View>
+      <Text style={styles.text}>Step By Step</Text>
       <StatusBar style="auto" />
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
+      <TouchableHighlight onPress={() => Alert.alert('Welcome')}>
+        <View>
+          <Image
+            source={{uri:'https://images.vexels.com/media/users/3/181775/isolated/preview/ff2947607b1af2e4a62a06537e292424-lightning-icon-by-vexels.png'}}
+            style={styles.thunder}
+          />
+        </View>
+      </TouchableHighlight>
+
     </View>
   
   );
@@ -21,6 +27,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#606',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  navbar: {
+    height: 200
+  }, 
+  text: {
+    fontSize: 50,
+    color: '#fff'
+  },
+  button: {
+    fontSize: 30,
+  },
+  thunder: {
+    marginTop: 45,
+    marginLeft: 20,
+    width: 320,
+    height: 320
+  }
 });
